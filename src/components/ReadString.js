7b714cd86ee8bf0ce19957ee257d42867f47ce9d
Waqas Mahmood <waqasnizamani3@gmail.com>
2021-06-16 23:16:14 +0500

    referral balance fixed

--- src/components/ReadString.js
+++ src/components/ReadString.js
@@ -6,6 +6,7 @@ export default function ReadString(props) {
     const [dataKey1, setdataKey1] = useState()
     const [dataKey2, setdataKey2] = useState()
     const [dataKey3, setdataKey3] = useState()
+    const [dataKey4, setdataKey4] = useState()
     
     useEffect(()=>{
         const { drizzle,drizzleState } = props;
@@ -18,24 +19,26 @@ export default function ReadString(props) {
         const dataKey1 = contract.methods["_holderBalances"].cacheCall(address);
         const dataKey2 = contract.methods["ethereumToTokens_"].cacheCall(100000000000);        
         const dataKey3 = contract.methods["dividendBalance"].cacheCall(address);
+        const dataKey4 = contract.methods["ReferralBalance"].cacheCall(address);
         // save the `dataKey` to local component state for later reference
         setdataKey( dataKey );
         setdataKey1(dataKey1)
         setdataKey2(dataKey2)
         setdataKey3(dataKey3)
+        setdataKey4(dataKey4)
     },[])
 
 
     const { GoldSeek3 } = props.drizzleState.contracts;
-    console.log("my string ", GoldSeek3)  
-
+   
   // using the saved `dataKey`, get the variable we're interested in
   const tokenPriceInitial_ = GoldSeek3.tokenPriceInitial_[dataKey];
   const balance = GoldSeek3._holderBalances[dataKey1];
   const rate = GoldSeek3.ethereumToTokens_[dataKey2];
   const dividendBalance = GoldSeek3.dividendBalance[dataKey3];
+  const referralBalance = GoldSeek3.ReferralBalance[dataKey4];
 
-  console.log("dividend",GoldSeek3.dividendBalance)
+
   function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").slice(0, 7);
 }
@@ -51,6 +54,7 @@ function numberWithCommas2(x) {
             <p>Current Token price is : {rate && rate.value/100000000000}</p>
             <p>Your <strong>Token Balance</strong>  is : {balance && numberWithCommas(balance.value/1000000000000000000) }</p>
             <p>Your <strong>Dividend Balance</strong>  is : {dividendBalance && numberWithCommas2(dividendBalance.value/1000000000000000000) }</p>
+            <p>Your <strong>ReferralBalance Balance</strong>  is : {referralBalance && numberWithCommas2(referralBalance.value/1000000000000000000) }</p>
         </div>
     )
 }
