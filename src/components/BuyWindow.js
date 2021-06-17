48175a001813f75ca0a71f44a003a311dd6a897c
Waqas Mahmood <waqasnizamani3@gmail.com>
2021-06-17 21:22:58 +0500

    buy function fixed

--- src/components/BuyWindow.js
+++ src/components/BuyWindow.js
@@ -5,6 +5,7 @@ export default function BuyWindow(props) {
     const [stackID, setStackID] = useState(null)
     const [referral,setReferral] = useState()
     const [amount,setAmount] = useState("")
+    const [YesRef,setYesRef] = useState(false)
     
     const [referralAdd,setReferralAdd] = useState("")
 
@@ -32,10 +33,10 @@ export default function BuyWindow(props) {
     const setValue = () => {
         const { drizzle, drizzleState } = props;
         const contract = drizzle.contracts.GoldSeek3;
-        console.log("contract",contract)
+       
         const { transactions, transactionStack } = props.drizzleState;
         // let drizzle know we want to call the `set` method with `value`
-        const stackId = contract.methods.buy.cacheSend(referralAdd, {
+        const stackId = contract.methods.buy.cacheSend(referralAdd? referralAdd: "0x0000000000000000000000000000000000000000", {
           from: drizzleState.accounts[0],
           value: drizzle.web3.utils.toWei(amount)
         })
@@ -49,6 +50,7 @@ export default function BuyWindow(props) {
         const referral = props.drizzleState.accounts[0]
         setInterval(() => {
           setReferral(referral)
+          setReferralAdd(null)
         }, 5000);  
 
       }
@@ -63,29 +65,35 @@ export default function BuyWindow(props) {
         // if transaction hash does not exist, don't display anything
         if (!txHash) return null;
 
-    
+        
         // otherwise, return the transaction status
         return `Transaction status: ${transactions[txHash] && transactions[txHash].status}`;
       };
-    
-    
+
+      console.log("referral",referralAdd)
+
       return (
         <div>
           <div><h1>This is buy window. </h1>
-{_referrerMapping =="0x0000000000000000000000000000000000000000"? 
-<h2>your referral is {_referrerMapping && _referrerMapping.value}</h2>:<h2>You dnt have any referral. would you like to give any referrer?</h2>}
+{_referrerMapping&&  _referrerMapping =="0x0000000000000000000000000000000000000000"? 
+<h2>your referral is {_referrerMapping && _referrerMapping.value}</h2>:<h2>You dnt have any referral. would you like to give any referrer?
+  <span><button onClick={()=>{setYesRef(true)}}>Yes</button></span>
+  <span><button onClick={()=>{setYesRef(false)}}>No</button></span>
+  </h2>}
           <label> Enter amount here <input value={amount} type="value"            
             onChange={({ target }) => setAmount(target.value)}/></label>
   
+            {YesRef? <label> Enter referral here <input value={referralAdd} type="text"            
+            onChange={({ target }) => setReferralAdd(target.value)}/></label>: null}
             <br/>
-            <label> Enter referral here <input value={referralAdd} type="text"            
-            onChange={({ target }) => setReferralAdd(target.value)}/></label>
+           
   
             <br/>
           </div>
         <button onClick={setValue}>Buy</button>
           <div>{getTxStatus()}</div>
           {referral? <p1>your referral link is : {`http://www.abc.com/${referral}`}</p1>:null}
+         
 
         </div>
       );
