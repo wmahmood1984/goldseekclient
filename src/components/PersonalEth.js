917dd16d12c629672cc216751bda0161211627b9
Waqas Mahmood <waqasnizamani3@gmail.com>
2021-06-19 11:38:05 +0500

    withdraw functions udpated

--- src/components/PersonalEth.js
+++ src/components/PersonalEth.js
@@ -3,7 +3,7 @@ import React,{useEffect,useState} from 'react'
 export default function PersonalEth(props) {
     const [dataKey, setdataKey] = useState()
     const [dataKey1, setdataKey1] = useState()
-
+    const [stackID, setStackID] = useState(null)
     const [dataKey2, setdataKey2] = useState()
     
     useEffect(()=>{
@@ -31,6 +31,70 @@ export default function PersonalEth(props) {
   const referralBalance = GoldSeek3.ReferralBalance[dataKey1];
   const _holderPersonalEth = GoldSeek3._holderPersonalEth[dataKey2];
 
+  const withdrawDividend = (amount) => {
+    const { drizzle, drizzleState } = props;
+    const contract = drizzle.contracts.GoldSeek3;
+   
+   
+    // let drizzle know we want to call the `set` method with `value`
+    const stackId = contract.methods.withdrawDividend.cacheSend(amount, {
+      from: drizzleState.accounts[0]
+    })
+     
+    // save the `stackId` for later reference
+    setStackID( stackId );
+
+  };
+
+  const withdrawReferral = (amount) => {
+    const { drizzle, drizzleState } = props;
+    const contract = drizzle.contracts.GoldSeek3;
+   
+   
+    // let drizzle know we want to call the `set` method with `value`
+    const stackId = contract.methods.withdrawrReferral.cacheSend(amount, {
+      from: drizzleState.accounts[0]
+    })
+     
+    // save the `stackId` for later reference
+    setStackID( stackId );
+
+  };
+
+  const withdrawPersonalEth = (amount) => {
+    const { drizzle, drizzleState } = props;
+    const contract = drizzle.contracts.GoldSeek3;
+   
+   
+    // let drizzle know we want to call the `set` method with `value`
+    const stackId = contract.methods.withdrawPersonalEth.cacheSend(amount, {
+      from: drizzleState.accounts[0]
+    })
+     
+    // save the `stackId` for later reference
+    setStackID( stackId );
+
+  };
+
+
+
+
+  const getTxStatus = () => {
+    // get the transaction states from the drizzle state
+    const { transactions, transactionStack } = props.drizzleState;
+    
+    // get the transaction hash using our saved `stackId`
+    const txHash = transactionStack[stackID];
+
+    // if transaction hash does not exist, don't display anything
+    if (!txHash) return null;
+
+
+    // otherwise, return the transaction status
+    return `Transaction status: ${transactions[txHash] && transactions[txHash].status}`;
+  };
+
+
 
   function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").slice(0, 7);
@@ -43,10 +107,13 @@ function numberWithCommas2(x) {
     return (
         <div>
         <p>            Your Dividend balance is {dividendBalance && numberWithCommas(dividendBalance.value/1000000000000000000) }</p><br/>
+        <button onClick={()=>{withdrawDividend(dividendBalance.value)}}>withdraw Dividend</button>
         <p>            Your referralBalance balance is {referralBalance && numberWithCommas(referralBalance.value/1000000000000000000) }</p><br/>
+        <button onClick={()=>{withdrawReferral(referralBalance.value)}}>withdraw referral</button>
         <p>     Your personal eth balance is {_holderPersonalEth && numberWithCommas(_holderPersonalEth.value/1000000000000000000) }</p><br/>
-
-
+        <button onClick={()=>{withdrawPersonalEth(_holderPersonalEth.value)}}>withdraw PersonaEth</button>
+<br/>
+{getTxStatus()}
  
         </div>
     )
