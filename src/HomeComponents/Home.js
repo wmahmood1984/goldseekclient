fe23280b9418cef34cb77172c293185f11da39db
Waqas Mahmood <waqasnizamani3@gmail.com>
2021-06-26 11:07:55 +0500

    some changes made in button

--- src/HomeComponents/Home.js
+++ src/HomeComponents/Home.js
@@ -28,24 +28,34 @@ export default function Home(props) {
         <div style={{marginTop:"200px"}}>
             <h1> Real Time Ethereum Dividends That Pay YOU Consistent Passive Income</h1>
             <p>protected by the blockchain, Seek Gold allows you 24 Hour Access To YOUR Ethereum Anytime You wish!</p>
-            {referrer? <Link to={`/main/:${referrer}`}>ENTER NOW</Link> : <Link to="/main">ENTER NOW</Link>}
+            {referrer? 
+            <Link 
+            style={{fontFamily:"sans-serif",fontSize:"16px",lineHeight:"24px",textDecoration:"none solid rgb(12,19,75)",textAlign:"center",wordSpacing:"0px",verticalAlign:"middle", backgroundColor:"#FFFFFF",backgroundPosition:"0% 0%", height:"53px",width:"192px",color:"#0C134B", border:"1px solid #FFFFFF",padding:"14px 12px 14px 12px",display:"inlineBlock",margin:"30px"}} 
+            to={`/main/:${referrer}`}>ENTER NOW</Link> 
+            
+            :<Link 
+            style={{border:"solid 1px black",backgroundImage:"90deg rgb(49 79 179)", backgroundColor:"black", color:"white"}}
+            to="/main">ENTER NOW</Link>}
 
             <h2>What Is Seek Gold?</h2>
             <p>Seek Gold is a global financial sharing and support community and part of the Seek Rewards/Seek Coin Ecosystem.Seek Gold was developed to provide anyone across the globe a way to create a dependable, virtually no risk way to create passive income while retaining 100% control over their Ethereum. We reward members who share the contract with othersand the members who choose to simply collect dividends with no requirements whatsoever. Many members will use the dividends created through Seek Gold to invest in Seek Coin and Seek Rewards to create additional passive daily income. We invite you to join us and start creating real time Ethereum Dividends TODAY!</p>
-            <div>
+            <div style={{display:"flex"}}>
+            <div style={{fontFamily:"sans-serif",fontSize:"16px",lineHeight:"24px",textDecoration:"none solid rgb(255,255,255)",textAlign:"center",wordSpacing:"0px",backgroundColor:"#020C2C",backgroundPosition:"0% 0%",height:"605px",width:"306px",color:"#ffffff",padding:"34px 22px 34px 22px",display:"block",margin:"30px"}}>
                 <h3>Real Time Dividends</h3>
                 <p>You get paid real time dividends when depositors buy, sell and transfer Seek Gold. The fee to buy Seek Gold is 17%. You will see 17?ducted immediately along with commissions and the platform fee. As soon as you deposit,10% is distributed immediately to all Seek Gold Holders including you. When you sell 7% is released to all Seek Gold Holders as well.The process of handling all funds including the distribution of Ethereum is completely managed by the immutable smart contract. There is no company, ceo or entity to run off with your money.</p>
             </div>
 
-            <div>
+            <div style={{fontFamily:"sans-serif",fontSize:"16px",lineHeight:"24px",textDecoration:"none solid rgb(255,255,255)",textAlign:"center",wordSpacing:"0px",backgroundColor:"#020C2C",backgroundPosition:"0% 0%",height:"605px",width:"306px",color:"#ffffff",padding:"34px 22px 34px 22px",display:"block",margin:"30px"}}>
                 <h3>How Much Can I Earn by Depositing Ethereum?</h3>
                 <p>You can earn Ethereum passively in three ways. 1) When people buy and sell Seek Gold YOU earn dividends in real time. 2) As the volume of people buying and selling increases the price to buy and sell Seek Gold will also rise. By monitoring the price periodically you can pick the perfect time to sell at a price h igher than you purchased for to receive increased profits! 3) As the price of Ethereum Rises so does the value of your Seek Gold! You get to benefit from all the appreciation of Ethereum on top of the other two profit streams. Most important is you ALWAYS retain instant access to all of your Ethereum at anytime at!</p>
             </div>
 
-            <div>
+            <div style={{fontFamily:"sans-serif",fontSize:"16px",lineHeight:"24px",textDecoration:"none solid rgb(255,255,255)",textAlign:"center",wordSpacing:"0px",backgroundColor:"#020C2C",backgroundPosition:"0% 0%",height:"605px",width:"306px",color:"#ffffff",padding:"34px 22px 34px 22px",display:"block",margin:"30px"}}>
                 <h3>Is my Ethereum Safe?</h3>
                 <p>Your Ethereum is as safe as it would be in your own crypto wallet because it is literally on the blockchain. Not only can you withdraw your dividends at anytime you also have the opportunity to redeposit your dividends for future growth. Your funds are never locked up, you can withdraw dividends and can cash out all or part of your Seek Gold at any time. With Seek Gold YOU are in 100% control over your Ethereum at all times!</p>
             </div>
+            </div>
+            
 
             <div>
                 <img src={logo}></img>
